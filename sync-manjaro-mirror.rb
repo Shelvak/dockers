2996202b6c9f0d2e25e0a5b931e36907500877d3
Néstor Coppi <nestorcoppi@gmail.com>
2025-02-15 17:34:20 -0300

    Sync manjaro-mirror

--- sync-manjaro-mirror.rb
+++ sync-manjaro-mirror.rb
@@ -8,22 +8,24 @@ require 'cgi'
 scrapper = 'https://manjaro.kurdy.org/stable'
 
 # Clean existing dir
-[
-  "*.db*"    ,
-  "*.tar.xz" ,
-  "*.tar.gz" ,
-  "*.tar.zst",
-  "*.sig"
-].each do |ext|
-  `find volumes/manjaro-mirror/ -name "#{ext}" -type f -delete`
-end
+# [
+#   "*.db*"    ,
+#   "*.tar.xz" ,
+#   "*.tar.gz" ,
+#   "*.tar.zst",
+#   "*.sig"
+# ].each do |ext|
+#   `find volumes/manjaro-mirror/ -name "#{ext}" -type f -delete`
+# end
 
 servers = [
+  'http://mirror.ibakerserver.pt/manjaro',
   'https://manjaro.kurdy.org',
+  'https://mirror.komelt.dev/manjaro',
+  'https://mirrors.aliyun.com/manjaro',
+  'https://mirrors.cicku.me/manjaro',
   'https://mirrors.manjaro.org/repo',
   'https://mirrors2.manjaro.org',
-  'http://mirror.ibakerserver.pt/manjaro/',
-  'https://mirrors.cicku.me/manjaro'
 ]
 
 `mkdir -p volumes/manjaro-mirror/stable/`
@@ -38,8 +40,8 @@ end
 full_files = []
 finished = false
 
-Thread.new do
-  %w[core extra community multilib].each do |repo|
+threads = %w[core extra community multilib].map do |repo|
+  Thread.new do
     puts "Downloading #{repo}"
     `mkdir -p volumes/manjaro-mirror/stable/#{repo}/x86_64/`
     raw_files = `curl -s #{scrapper}/#{repo}/x86_64/`
@@ -52,20 +54,27 @@ Thread.new do
     i = 0
     files.map {|e| e&.gsub('./', '') }.compact.uniq.each do |e|
       # file already exists
-      next if system("[ -f volumes/manjaro-mirror/stable/#{repo}/x86_64/#{Shellwords.escape(CGI.unescape(e))} ]")
+      if !e.match?(/\.db/) && system("[ -f volumes/manjaro-mirror/stable/#{repo}/x86_64/#{Shellwords.escape(CGI.unescape(e))} ]")
+        # puts "Skipping #{e}"
+        next
+      end
 
       # Remove old version
       if pkg_name = e.match(/(\S+)-\d+\.\w+\.\d+/)&.captures&.first
-        puts "rm volumes/manjaro-mirror/stable/#{repo}/x86_64/#{Shellwords.escape(CGI.unescape(pkg_name))}*"
+        # puts "rm volumes/manjaro-mirror/stable/#{repo}/x86_64/#{Shellwords.escape(CGI.unescape(pkg_name))}*"
         `rm -f volumes/manjaro-mirror/stable/#{repo}/x86_64/#{Shellwords.escape(CGI.unescape(pkg_name))}*`
       end
 
       full_files << "stable/#{repo}/x86_64/#{Shellwords.escape(CGI.unescape(e))}"
     end
   end
+end
+
+Thread.new do
+  threads.each(&:join)
   finished = true
 end
 
-Parallel.each(-> { full_files.pop || (finished && Parallel::Stop) }, in_threads: 50) do |e|
+Parallel.each(-> { full_files.pop || (finished && Parallel::Stop) }, in_threads: 100) do |e|
   `wget --no-check-certificate --inet4-only -q -nc #{servers.sample}/#{e} -O volumes/manjaro-mirror/#{e}`
 end
